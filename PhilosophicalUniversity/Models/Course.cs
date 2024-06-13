1ea13923476ea1ef80d6854d3f2b2852307f8549
nvivek42 <viveknarole100@gmail.com>
2024-06-13 19:12:30 +0530

    entity framework implementation

---
+++ PhilosophicalUniversity/Models/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace PhilosophicalUniversity.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
---
+++ PhilosophicalUniversity/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PhilosophicalUniversity.Data;
+using PhilosophicalUniversity.Models;
+
+namespace PhilosophicalUniversity.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly PhilosophicalUniversity.Data.SchoolContext _context;
+
+        public IndexModel(PhilosophicalUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Courses.ToListAsync();
+        }
+    }
+}
